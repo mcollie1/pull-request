fe404f497b2cabaf63736ba1239f862716b11f95
mcollie1 <marcus.collier7@yahoo.com>
2014-05-04 19:00:08 -0400

    First commit and README udpate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PullRequest::Application.config.secret_key_base = '736101f11ce2f0cdfb3221ce070913dd9b57c77780c8ff16aa6f222e7177ce76870f4bcdfb4a2c6909d1aee524f38300ceaca91c4f654938c37d944750f605e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PullRequest::Application.config.session_store :cookie_store, key: '_pull-request_session'
